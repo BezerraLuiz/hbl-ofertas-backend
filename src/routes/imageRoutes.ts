5ce114d95aff0276e74be40d902f50ed7602f2f7
luiz.bezerra <luizantoniopcb@gmail.com>
2024-11-16 03:16:56 -0300

    teste

--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -3,4 +3,4 @@ import { uploadImageHandler } from '../controllers/imageController';
 
 export default async function imageRoutes(server: FastifyInstance) {
   server.post('/upload-image', uploadImageHandler);
-}
+};
