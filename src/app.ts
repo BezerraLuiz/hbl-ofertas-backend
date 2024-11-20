17a76f0c750ac9bf3612ad2bf22a65eb8fe06db9
luiz.bezerra <luizantoniopcb@gmail.com>
2024-11-19 22:13:19 -0300

    Adicionado logs no app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,52 +5,67 @@ import fastifyMultipart from "@fastify/multipart";
 import fastifyStatic from "@fastify/static";
 import dotenv from "dotenv";
 import path from "path";
-import { authenticate } from "./middlewares/AuthenticateMiddleware";
+import { authenticate } from "./middlewares/authenticateMiddleware";
 import { usuariosRoutes } from "./routes/userRoutes";
 import { productsRoutes } from "./routes/productsRoutes";
 import imageRoutes from "./routes/imageRoutes";
 
 dotenv.config();
 
+// Logando para verificar se a variável de ambiente foi carregada corretamente
+console.log(`[${new Date().toISOString()}] ✅ Variáveis de ambiente carregadas.`);
+
 export const server = fastify({ logger: true });
+
+console.log(`[${new Date().toISOString()}] 🛠️ Inicializando servidor Fastify...`);
 
 server.register(cors, {
   origin: "*",
   credentials: true,
   preflight: true,
 });
+console.log(`[${new Date().toISOString()}] ✅ Plugin CORS registrado com sucesso!`);
 
 server.register(fastifyMultipart);
+console.log(`[${new Date().toISOString()}] ✅ Plugin Multipart registrado com sucesso!`);
 
 server.register(fastifyJwt, {
   secret: process.env.JWT_SECRET as string,
 });
+console.log(`[${new Date().toISOString()}] ✅ Plugin JWT registrado com sucesso!`);
 
+console.log(`[${new Date().toISOString()}] 🔑 Registrando middleware de autenticação...`);
 authenticate(server);
-
-server.register(usuariosRoutes);
-server.register(imageRoutes);
-server.register(productsRoutes);
 
 server.register(fastifyStatic, {
   root: path.join(__dirname, "../uploads"),
   prefix: "/uploads/",
 });
+console.log(`[${new Date().toISOString()}] ✅ Plugin de arquivos estáticos registrado com sucesso!`);
 
-// Rota para teste do server.
+console.log(`[${new Date().toISOString()}] 🛣️ Registrando rotas...`);
+server.register(usuariosRoutes);
+server.register(imageRoutes);
+server.register(productsRoutes);
+
 server.get("/", async () => {
-  return console.log("Rota Funcionando!");
+  // Logando quando a rota de teste é acessada
+  console.log(`[${new Date().toISOString()}] 🌐 Acessada a rota de teste!`);
+  return { message: "Rota Funcionando!" };
 });
 
 const port = parseInt(process.env.PORT || "3333", 10);
+
+// Logando quando o servidor está tentando iniciar
+console.log(`[${new Date().toISOString()}] 🚀 Tentando iniciar servidor na porta ${port}...`);
 
 server
   .listen({ port })
   .then(() => {
     console.log(
-      `🚀 HTTP server running on https://hbl-ofertas-backend.vercel.app/`
+      `[${new Date().toISOString()}] 🚀 Servidor HTTP iniciado com sucesso em https://hbl-ofertas-backend.vercel.app/`
     );
   })
   .catch((err) => {
-    console.error(err);
+    console.error(`[${new Date().toISOString()}] ❌ Erro ao iniciar o servidor:`, err);
   });
