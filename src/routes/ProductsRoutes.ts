147e80d5ab54c677016cf7d600960fae27d56267
luiz.bezerra <luizantoniopcb@gmail.com>
2024-12-03 01:14:48 -0300

    Adjusting existProduct return

--- src/routes/ProductsRoutes.ts
+++ src/routes/ProductsRoutes.ts
@@ -3,8 +3,8 @@ import { server } from "../Server";
 
 export async function productsRoutes() {
   server.get("/products", getAllProductsHandler);
-  server.get("/products:sku", getProductBySkuHandler);
-  server.delete("/products/delete:sku", deleteProductHandler);
+  server.get("/products/sku", getProductBySkuHandler);
+  server.delete("/products/delete/sku", deleteProductHandler);
   server.post("/products", createProductHandler);
   server.put("/products/update", UpdateProductHandler);
 }
