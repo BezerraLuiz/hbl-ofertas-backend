6e8c33fbd42097f7bb78512677074ee89430ba9c
luiz.bezerra <luizantoniopcb@gmail.com>
2024-12-03 20:45:02 -0300

    alt routes and migrate prisma dev

--- src/routes/ProductsRoutes.ts
+++ src/routes/ProductsRoutes.ts
@@ -3,8 +3,8 @@ import { server } from "../Server";
 
 export async function productsRoutes() {
   server.get("/products", getAllProductsHandler);
-  server.get("/products/sku", getProductBySkuHandler);
-  server.delete("/products/delete/sku", deleteProductHandler);
+  server.get("/products/get", getProductBySkuHandler);
+  server.delete("/products/delete", deleteProductHandler);
   server.post("/products", createProductHandler);
   server.put("/products/update", UpdateProductHandler);
 }
